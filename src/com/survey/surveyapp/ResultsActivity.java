d8723dfbee7f8e1b92641f8aa9d2b6041cc21dc7
Paweł <pnocon@onet.pl>
2015-01-16 12:43:29 +0100

    ResultsActivity

---
+++ src/com/survey/surveyapp/ResultsActivity.java
@@ -0,0 +1,27 @@
+package com.survey.surveyapp;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class ResultsActivity extends ActionBarActivity {
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_results);
+		
+		ListView listView1 = (ListView) findViewById(R.id.results_ListView);
+        
+        String[] items = {  };
+        
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                    android.R.layout.simple_list_item_1, items);
+        
+        listView1.setAdapter(adapter);
+	}
+	
+	
+	
+}
