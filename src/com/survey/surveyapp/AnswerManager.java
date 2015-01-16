b5103c2af2e29e1aee93145a3b7f53d108192924
pw90 <pw90@onet.eu>
2015-01-16 12:03:53 +0100

    Results
    Adding results to database

--- src/com/survey/surveyapp/AnswerManager.java
+++ src/com/survey/surveyapp/AnswerManager.java
@@ -7,24 +7,34 @@ public class AnswerManager {
 	
 	private SQLiteDatabase db;
 	
-	public void addNewAnswer(Result result, Question question, Answer answer) {
+	public void addNewAnswer(Answer answer, Result result, int questionIndex) {
 		
 		int numberOfAnswers;
-		int numberOfQuestions = result.getQuestions().size();
+		numberOfAnswers = result.getInstance().getQuestion(questionIndex).getAnswers().size();
 		
-		for (int i = 0; i < numberOfQuestions; i++) {
-			numberOfAnswers = result.getQuestion(i).getAnswers().size();
-			for (int j = 0; j < numberOfAnswers; j++) {
-				insertAnswer(answer, result.getQuestion(i));
-			}
-		}		
+		for (int j = 0; j < numberOfAnswers; j++) {
+			insertAnswer(answer.getId(), answer.getValue(), result.getQuestion(questionIndex).getId());
+		}	
 	}
 	
-	public void insertAnswer(Answer answer, Question question) {
+	public void insertAnswer(int answerId, boolean answerValue, int questionId) {
 		ContentValues cv = new ContentValues();
-		cv.put("ID_Odp", answer.getId());
-		cv.put("ID_RezPyt", question.getId());
-		cv.put("Wynik", answer.getValue());
+		cv.put("ID_Odp", answerId);
+		cv.put("ID_RezPyt", questionId);
+		cv.put("Wynik", answerValue);
 		db.insert("RezOdpowiedz", null, cv);		
 	}	
+	
+	public void insertKeys(int questionId, int topicId, int topicIdRez) {
+		ContentValues cv = new ContentValues();
+		cv.put("ID_Pyt", questionId);
+		cv.put("ID_RezTem", topicIdRez);
+		db.insert("RezPytanie", null, cv);
+		cv.clear();
+		
+		cv.put("ID_Tem", topicId);
+		db.insert("RezTemat", null, cv);
+		cv.clear();		
+	}
+	
 }
