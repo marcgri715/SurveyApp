76236063f73a42fd54cb9802bba0621af00583bc
Nikusiaczek <agnieszka.wesolowska1@gmail.com>
2015-01-29 11:45:51 +0100

    moje zmiany

---
+++ src/com/survey/surveyapp/SingleAnswerItem.java
@@ -0,0 +1,10 @@
+package com.survey.surveyapp;
+
+import android.widget.CheckBox;
+import android.widget.EditText;
+
+public class SingleAnswerItem {
+
+	Boolean chk_state;
+    String answerText;
+}
