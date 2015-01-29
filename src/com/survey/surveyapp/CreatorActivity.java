6a68200ff74fc27f424393c5a5060cbd3527a586
marcgri715 <marcgri715@student.polsl.pl>
2015-01-29 11:45:15 +0100

    onclicklistener

---
+++ src/com/survey/surveyapp/CreatorActivity.java
@@ -0,0 +1,62 @@
+package com.survey.surveyapp;
+
+import java.util.ArrayList;
+
+import android.support.v7.app.ActionBarActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnKeyListener;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+
+public class CreatorActivity extends ActionBarActivity implements OnKeyListener {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_creator_main);
+		Result.getInstance().setContent("");
+		Result.getInstance().setQuestions(new ArrayList<Question>(1));
+		Result.getInstance().getQuestion(0).setAnswers(new ArrayList<Answer>(1));
+		EditText editText = (EditText) findViewById(R.id.et_topic);
+		editText.setOnKeyListener(this);
+	}
+	
+	@Override
+	public boolean onKey(View view, int keyCode, KeyEvent event) {
+		EditText editText = (EditText) view;
+		if (keyCode == EditorInfo.IME_ACTION_DONE ||
+		 keyCode == EditorInfo.IME_ACTION_SEARCH || 
+		 event.getAction() == KeyEvent.ACTION_DOWN &&
+		 event.getKeyCode() == KeyEvent.KEYCODE_ENTER) {
+			Result.getInstance().setContent(editText.getText().toString());
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.creator, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+}
